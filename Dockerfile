b04ac583ceac6ab58bc5b2d01de6cc9b7c2dec80
JJ Macalinao <i@jmac.ph>
2021-04-28 13:41:54 +0800

    Use my fork; add line type option

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,16 @@ FROM ubuntu:18.04 as build
 ENV INFLUXDB_DB="speedtest" \
     INFLUXDB_URL="http://influxdb:8086" \
     SPEEDTEST_HOST="local" \
-    SPEEDTEST_INTERVAL=3600
+    SPEEDTEST_INTERVAL=3600 \
+    LINE_TYPE="auto"
 
 WORKDIR /app
 
 RUN apt-get update \
     && apt-get install build-essential libcurl4-openssl-dev libxml2-dev libssl-dev cmake git jq curl -y \
     && rm -rf /var/lib/apt/lists/* 
-RUN git clone https://github.com/taganaka/SpeedTest \
-    && cd SpeedTest \
+RUN git clone https://github.com/JMacalinao/speedtestplusplus \
+    && cd speedtestplusplus \
     && cmake -DCMAKE_BUILD_TYPE=Release . \
     && make install
 
--- run.sh
+++ run.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 while :; do
-	if [[ -z "${TESTSERVER}" ]]; then
+	if [[ -z "${SPEEDTEST_SERVER_URL}" ]]; then
 	 	echo "[Info][$(date)] Starting SpeedTest++..."
-		JSON=$(./SpeedTest/SpeedTest  --output json)
+		JSON=$(./speedtestplusplus/SpeedTest --output json --share --line-type ${LINE_TYPE})
 	else
- 		echo "[Info][$(date)] Starting SpeedTest++ with specific testerver ${TESTSERVER}..."
- 		JSON=$(./SpeedTest/SpeedTest --test-server=${TESTSERVER} --output json)
+ 		echo "[Info][$(date)] Starting SpeedTest++ with specific testerver ${SPEEDTEST_SERVER_URL}..."
+ 		JSON=$(./speedtestplusplus/SpeedTest --test-server=${SPEEDTEST_SERVER_URL} --output json --share)
 	fi
 	DOWNLOAD=$(echo ${JSON} | jq -r .download)
 	UPLOAD=$(echo ${JSON} | jq -r .upload)
 	PING=$(echo ${JSON} | jq -r .ping)
 	JITTER=$(echo ${JSON} | jq -r .jitter)
+	SHARE=$(echo ${JSON} | jq -r .share)
 	UPLOAD=$(echo $UPLOAD | sed 's/\(\.[0-9][0-9]\)[0-9]*/\1/g')
 	DOWNLOAD=$(echo $DOWNLOAD | sed 's/\(\.[0-9][0-9]\)[0-9]*/\1/g')
-	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Jitter: ${JITTER}"
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Jitter: ${JITTER}, Share: ${SHARE}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${SPEEDTEST_HOST} value=${PING}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "jitter,host=${SPEEDTEST_HOST} value=${JITTER}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "share,host=${SPEEDTEST_HOST} value=${SHARE}"
 	echo "[Info][$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
 	sleep ${SPEEDTEST_INTERVAL}
 done
